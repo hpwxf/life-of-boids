038493e28f356f350b03245db7fb838069bcfc2c
Pascal HAVÉ <hpwxf@haveneer.com>
2021-04-04 19:57:38 +0200

    Initial C++ project
    Do nothing, but it does it well !

---
+++ cpp/README.md
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+
+# CMake options
+
+* `-DENABLE_STATIC_ANALYSIS=ON|OFF` : enable/disable static analysis while compiling
+
+* `-DCMAKE_CXX_COMPILER_LAUNCHER=ccache` : enable `ccache` as compiler cache
+
+
---
+++ cpp/cmake/ClangFormatProcess.cmake
@@ -0,0 +1,14 @@
+# --------------- runs clang-format in place using the style file ---------------
+
+if(CMAKE_SOURCE_DIR AND CLANG_FORMAT)
+    # get C++ sources file list (ignoring packages)
+    file(GLOB_RECURSE ALL_SOURCE_FILES
+            ${PROJECT_SOURCE_DIR}/src/**.[hc]pp
+            ${PROJECT_SOURCE_DIR}/tests/**.[hc]pp
+            )
+
+    # apply style to the file list
+    foreach(SOURCE_FILE ${ALL_SOURCE_FILES})
+        execute_process(COMMAND "${CLANG_FORMAT}" -style=file -verbose -i "${SOURCE_FILE}")
+    endforeach()
+endif()
